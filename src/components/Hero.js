d22a65d7f282775465e687971bf518d764dc6fcf
Arief Setiawan <arief.setiawan8788@gmail.com>
2021-12-16 15:18:29 +0700

    set up setting & create hero

---
+++ src/components/Hero.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Logo from "../Assets/img/Logo.png";
+
+const Hero = () => {
+  return (
+    <section className="hero-section">
+      <div className="container">
+        <div className="row-hero">
+          <div className="img-wrapper-hero">
+            <img className="logo-hero" src={Logo} alt="" />
+          </div>
+          <div className="input-wrapper-hero">
+            <input type="text" className="input-hero"/>
+            <button type="submit" className="btn">Search</button>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
